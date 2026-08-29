f92e0d5263eb84db471a1a051bc0b65e056892c8
Falkreon <falkreon@gmail.com>
2026-08-28 22:54:03 -0500

    Complete Display sql (#29)

---
+++ database/Display.sql
@@ -0,0 +1,21 @@
+/*
+ * CSD 460: Capstone in Software Development
+ * Module 4: Database
+ * Gold Team
+ *  Isaac Ellingson
+ *  Patrice Moracchini
+ *  Cannon Rivera
+ *  José Velázquez Sáenz
+ * 8/30/2026
+ *
+ * Displays the contents of every table in the MoffatBay (lodge) database
+ */
+
+USE MoffatBay;
+
+SELECT * FROM Role;
+SELECT * FROM Permission;
+SELECT * FROM User;
+SELECT * FROM Reservation;
+SELECT * FROM ContactMessage;
+SELECT * FROM RolePermission;
