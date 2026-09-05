339fde4261cc0d2c6eaba7963dbba45a5adbcdef
Falkreon <falkreon@gmail.com>
2026-09-04 22:02:30 -0500

    User Registration (#39)
    * Work on registration form
    
    * Improve validation, validation feedback, and button enable/disable
    
    * More fussing with email and phone regex, finish frontend
    
    * Make registration work. Update database design and impl to reflect PhoneNumber field
    
    * Finish account registration system

---
+++ src/do_register.php
@@ -0,0 +1,132 @@
+<?php declare(strict_types=1);
+session_start();
+
+/**
+ * CSD460: Capstone in Software Development
+ * Gold Team
+ *   Isaac Ellingson
+ *   Patrice Moracchini
+ *   Cannon Rivera
+ *   José Velázquez Sáenz
+ * 9/6/2026
+ */
+
+require_once('database_capability.php');
+const ROLE_CUSTOMER = 2;
+
+function validateFormData(): array|false {
+	// All form fields are required. If any are missing or null we should bail
+	if (!array_key_exists('email', $_POST)) return false;
+	if (!array_key_exists('firstName', $_POST)) return false;
+	if (!array_key_exists('lastName', $_POST)) return false;
+	if (!array_key_exists('phone', $_POST)) return false;
+	if (!array_key_exists('password', $_POST)) return false;
+
+	if ($_POST['email'] === null) return false;
+	if ($_POST['firstName'] === null) return false;
+	if ($_POST['lastName'] === null) return false;
+	if ($_POST['phone'] === null) return false;
+	if ($_POST['password'] === null) return false;
+
+	$result = [];
+	$user = new User();
+
+	// Apply same validation rule from the form field, in case the user has manually
+	// inserted the fields into the URL
+	if (!preg_match('/^[^\s@]+@[^\s@]+\.[^\s@]+$/', $_POST['email'])) return false;
+	$user->Email = $_POST['email'];
+
+	if (preg_match('/^(?:(?:\(([0-9]{3})\)[ ]?)|(?:([0-9]{3})[ ]?))?([0-9]{3})[ ]?-?[ ]?([0-9]{4})$/', $_POST['phone'], $matches)) {
+		if ($matches[1] === '' && $matches[2] === '') {
+			$areacode = '';
+		} else {
+			if ($matches[1] !== '') {
+				$areacode = '(' . $matches[1] . ') ';
+			} else {
+				$areacode = '(' . $matches[2] . ') ';
+			}
+		}
+
+		// TODO: Phone PCRE is broken! Only one area code form works!
+		$user->PhoneNumber = $areacode . $matches[3] . '-' . $matches[4];
+	} else {
+		return false;
+	}
+
+	if ($_POST['firstName'] == '') return false;
+	if ($_POST['lastName'] == '') return false;
+	$user->FirstName = $_POST['firstName'];
+	$user->LastName = $_POST['lastName'];
+
+	$user->RoleId = ROLE_CUSTOMER;
+
+	//The password is the trickiest part!
+	if (!preg_match(
+		'/^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,}$/',
+		$_POST['password']
+	)) return false;
+	$result['password'] = $_POST['password'];
+	$result['user'] = $user;
+
+	return $result;
+}
+
+
+
+$form = validateFormData();
+if ($form) {
+	$db = new ReadWriteCapability();
+	{
+		$id = $db->createUser($form['user'], $form['password']);
+		if ($id === false) {
+			?>
+			<script>
+				window.location.replace("register_error.php");
+			</script>
+			<?php
+		} else {
+			// Set user session - they are now logged in!
+			$_SESSION['user_id'] = $id;
+
+			// TODO: If we make a user-home page, switch this to redirect to it.
+			?>
+
+			<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Moffat Bay Lodge - Register</title>
+				<link rel="preconnect" href="https://fonts.googleapis.com">
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+				<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+				<link rel="stylesheet" href="register.css">
+			</head>
+			<body>
+				<section>
+					<h1>Account Created</h1>
+					<p>Please wait while we redirect you.
+					<div class="buttons">
+						<a class="button callout-button" href="index.php">Continue</a>
+					</div>
+				</section>
+			</body>
+			<script>
+				window.location.replace("index.php");
+			</script>
+			</html>
+
+			<?php
+		}
+
+	}
+	unset($db);
+} else {
+	// Form validation failure. Generally the user did something rude here!
+	?>
+	<script>
+		window.location.replace("register_error.php");
+	</script>
+	<?php
+}
+?>
---
+++ src/register.php
@@ -0,0 +1,133 @@
+<?php declare(strict_types=1);
+session_start();
+
+/**
+ * CSD460: Capstone in Software Development
+ * Gold Team
+ *   Isaac Ellingson
+ *   Patrice Moracchini
+ *   Cannon Rivera
+ *   José Velázquez Sáenz
+ * 9/6/2026
+ */
+
+// See logout.php for notes on this block
+$_SESSION = [];
+if (ini_get("session.use_cookies")) {
+	$params = session_get_cookie_params();
+	setcookie(session_name(), '', time() - 42000,
+		$params["path"], $params["domain"],
+		$params["secure"], $params["httponly"]
+	);
+}
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Moffat Bay Lodge - Register</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="register.css">
+	<script>
+	const VALID_FAIL = "validation-fail";
+
+	function onPasswordInput() {
+		var pass = document.getElementById("password").value;
+
+		var req_8ch = document.getElementById("req-8ch");
+		if (pass.length >= 8) {
+			req_8ch.classList.remove(VALID_FAIL);
+		} else {
+			req_8ch.classList.add(VALID_FAIL);
+		}
+
+		var req_upp = document.getElementById("req-upp");
+		if (pass.match(/[A-Z]/g)) {
+			req_upp.classList.remove(VALID_FAIL);
+		} else {
+			req_upp.classList.add(VALID_FAIL);
+		}
+
+		var req_low = document.getElementById("req-low");
+		if (pass.match(/[a-z]/g)) {
+			req_low.classList.remove(VALID_FAIL);
+		} else {
+			req_low.classList.add(VALID_FAIL);
+		}
+
+		var req_num = document.getElementById("req-num");
+		if (pass.match(/[0-9]/g)) {
+			req_num.classList.remove(VALID_FAIL);
+		} else {
+			req_num.classList.add(VALID_FAIL);
+		}
+		updateButton();
+	}
+
+	function updateButton() {
+		let valid = true;
+		valid &= document.getElementById("email").checkValidity();
+		valid &= document.getElementById("firstName").checkValidity();
+		valid &= document.getElementById("lastName").checkValidity();
+		valid &= document.getElementById("phone").checkValidity();
+		valid &= document.getElementById("password").checkValidity();
+		document.getElementById("register").disabled = !valid;
+	}
+	</script>
+</head>
+<body>
+	<section>
+		<h1>Create an Account</h1>
+		<p>(or <a href="login.php">Log In</a> instead)
+		<form class="form-2col" method="POST" action="do_register.php">
+			<p><label for="email">Email Address</label>
+			<!-- Extremely simple and permissive email regex: -->
+			<!-- ^[^\s@]+@[^\s@]+\.[^\s@]+$ -->
+
+			<!-- Nonworking - Attempted to use a more robust regex, which fails in-browser but not in regexr: -->
+			<!-- (?:[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*|&quot;(?:[!#$%&'*+/=?^_`{|}~\-\x20-\x7E]|\\[!#$%&'*+/=?^_`{|}~\-\x20-\x7E])*&quot;)@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])? -->
+			   <input type="text" name="email" id="email"
+			   pattern="^[^\s@]+@[^\s@]+\.[^\s@]+$"
+			   oninput="updateButton();"
+			   required
+			   ></input>
+			<p><label for="firstName">First Name</label>
+			   <input type="text" name="firstName" id="firstName"
+			   oninput="updateButton();"
+			   required
+			   ></input>
+			<p><label for="lastName">Last Name</label>
+			   <input type="text" name="lastName" id="lastName"
+			   oninput="updateButton();"
+			   required
+			   ></input>
+			<p><label for="phone">Telephone</label>
+			   <input type="text" name="phone" id="phone"
+			   pattern="^(?:(?:\(([0-9]{3})\)[ ]?)|(?:([0-9]{3})[ ]?))?([0-9]{3})[ ]?-?[ ]?([0-9]{4})$"
+			   oninput="updateButton();"
+			   required
+			   ></input>
+			<p><label for="password">Password</label>
+			   <input type="password" name="password" id="password"
+			   pattern="(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,}"
+			   oninput="onPasswordInput();"
+			   required
+			   ></input>
+
+			<p class="req validation-fail" id="req-8ch">Must be at least 8 characters
+			<p class="req validation-fail" id="req-num">Must include one number
+			<p class="req validation-fail" id="req-upp">Must include one uppercase letter
+			<p class="req validation-fail" id="req-low">Must include one lowercase letter
+			<div class="buttons">
+				<a class="button" href="index.php">Cancel</a>
+				<input type="submit" value="Register" id="register" onclick="" disabled>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
